eb5c1be87932c9f0a3efced6e9787e1288f2d46d
mbohbless <mbohblesspearl@gmail.com>
2023-08-14 00:49:25 +0300

    implementations of the stock file

--- order_management/order_management/stock.py
+++ order_management/order_management/stock.py
@@ -2,49 +2,84 @@ import json
 from typing import List
 from .product import Product
 
+
 class Stock:
     """Represents the catalog of products
-    
+
     Attributes:
         products: the list of products
     """
+
     def __init__(self, products: List[Product]) -> None:
         self.products = products
 
-    def update(self, id: int, change: int):
+    def update(self, id: str, change: int):
         """Update the quantity of a product by adding or removing
-        
+
         Args:
             id: identifier of the product
             change: the value by which the quantity should be update (+1 adds 1, -2 removes 2 for example)
         """
-        #TODO: Make sure the product exists, and that by making the change, the value is still >= 0
-        
-        #TODO: Update the quantity
+        # TODO: Make sure the product exists, and that by making the change, the value is still >= 0
+        for product in self.products:
+            if product.code == id:
+                product.quantity += change
+                return
+
+
+        # TODO: Update the quantity
 
     def getProductByID(self, id: str) -> Product:
         """Gets a product by its ID
         Args:
             id: identifier of the product
-        
+
         Returns: the product's object
         """
-        #TODO: Implement te function
-        
+        for product in self.products:
+            if product.code == id:
+                return product
+        return None
+
     def dump(self, outfile: str):
         """Saves the stock to a JSON file"""
-        #TODO: Implement the function
-    
+        # TODO: Implement the function
+        with open(outfile, 'w') as f:
+            json.dump(self.products, f)
+            
+
     @staticmethod
     def load(infile: str):
         """Loads the stock from an existing file
-        
+
         Args: 
             infile: input file to the function
         """
-        #TODO: Implement the function
-    
+        with open(infile, 'r') as f:
+            data = json.load(f)
+            print(data)
+            products = [
+                Product(
+                    code=product['code'],
+                    name=product['name'],
+                    brand=product['brand'],
+                    description=product['description'],
+                    quantity=product['quantity'],
+                    price=product['price'],
+                    requires_prescription=product['requires_prescription'],
+                    brand=product['brand'],
+                    category=product['category'],
+                    dosage_instruction=product['dosage_instruction'],
+
+
+                )
+                for product in data
+            ]
+            
+            return Stock(products)
+        # TODO: Implement the function
+
     def __str__(self) -> str:
         """Returns a string representation of the stock
         """
-        #TODO: Return the description of the stock with a nice output showing the ID, Name, Brand, Description, Quantity, Price, and the requires_prescription field
+        # TODO: Return the description of the stock with a nice output showing the ID, Name, Brand, Description, Quantity, Price, and the requires_prescription field
